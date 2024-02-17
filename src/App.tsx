6beb1101f356aa824f0d942fed14dde9f041d9b8
Igor <iigorfelipe@gmail.com>
2024-02-16 21:55:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { GIFS_AND_LINKS } from './helpers/gifs';
+import './styles.css';
+
+const App: React.FC = () => {
+  const [currentPainel, setCurrentPainel] = useState<'1' | '2' | '3'>('1');
+
+  const fuja = () => {
+    const botaoNao = document.getElementById('nao');
+
+    if (botaoNao) {
+      const larguraJanela = window.innerWidth;
+      const alturaJanela = window.innerHeight;
+
+      const maxX = larguraJanela - (botaoNao.offsetWidth || 0);
+      const maxY = alturaJanela - (botaoNao.offsetHeight || 0);
+
+      const aleatorioX = Math.floor(Math.random() * maxX);
+      const aleatorioY = Math.floor(Math.random() * maxY);
+
+      if (botaoNao.style) {
+        botaoNao.style.left = aleatorioX + 'px';
+        botaoNao.style.top = aleatorioY + 'px';
+      }
+    }
+  };
+
+  const exibirPedido = () => {
+    return (
+      <div className="conteudo1">
+        <h2 className="title1">Feliz Dia dos Namorados</h2>
+
+        <a
+          className="main-link"
+          href="https://www.youtube.com/watch?v=D2tl4ZQZE44"
+          target="_blank"
+        >
+          <img
+            className="main-gif"
+            src="https://tenor.com/pt-BR/view/tkthao219-bubududu-gif-25153986.gif"
+            alt="Gif"
+          />
+        </a>
+
+        <div className="footer1">
+          <h3 className="title2">Aceita continuar namorando comigo? </h3>
+
+          <div className="divButtons">
+            <button id="sim" onClick={() => setCurrentPainel('2')}>
+              Sim!
+            </button>
+            <button id="nao" onMouseOver={fuja}>
+              Não!
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const exibirVideo = () => {
+    return (
+      <video width="500px" height="500px" controls className="video">
+        <source src="/videos/op.mp4" type="video/mp4" />
+      </video>
+    );
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="div1">
+        <div className="gif-container">
+          {GIFS_AND_LINKS.dudus.map(({ gif, link }) => (
+            <a key={link + gif} href={link} target="_blank" className="gifs">
+              <img src={gif} alt="Gif Bubu" />
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <div className="div2">
+        <div className="painel">
+          {currentPainel === '1' ? exibirPedido() : exibirVideo()}
+        </div>
+      </div>
+
+      <div className="div3">
+        <div className="gif-container">
+          {GIFS_AND_LINKS.cats.map(({ gif, link }) => (
+            <a key={link + gif} href={link} target="_blank" className="gifs">
+              <img src={gif} alt="Gif Cat" />
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/helpers/gifs.ts
@@ -0,0 +1,55 @@
+export const GIFS_AND_LINKS = {
+  dudus: [
+    { 
+      gif: "https://tenor.com/pt-BR/view/dudububu-kiss-gif-3311649274909958771.gif",
+      link: "https://www.youtube.com/watch?v=beqxNLUT5R0",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/i-love-you-love-radi-semi-radi-and-semi-gif-9258996247025425021.gif",
+      link: "https://www.youtube.com/watch?v=eihpkV7KSKo",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/bubu-dudu-kisses-too-cute-cute-kiss-gif-8114411598979074869.gif",
+      link: "https://www.youtube.com/watch?v=l-DFmaVUzJY",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/dance-gif-2695183123323868423.gif",
+      link: "https://www.youtube.com/watch?v=oVBbtjj6hJU",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/bubu-dudu-heart-gif-4013167120558620298.gif",
+      link: "https://www.youtube.com/watch?v=o9faNnjq4Tg",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/cute-bubu-dudu-good-night-gif-10061420666862849924.gif",
+      link: "https://www.youtube.com/watch?v=RvehBiepP5U",
+    },
+  ],
+
+  cats: [
+    { 
+      gif: "https://tenor.com/pt-BR/view/meo-meo-meow-cat-talking-cute-cattoo-gif-1905059869731627022.gif",
+      link: "https://www.instagram.com/reel/C2Psh5mrANS/",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/cat-kitty-cute-stand-gif-17289064.gif",
+      link: "https://pbs.twimg.com/media/GFX_ky5XYAAegx6?format=jpg&name=900x900",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/cat-gif-6997856904020830598.gif",
+      link: "https://pbs.twimg.com/media/GE77ZgYWUAANcH7?format=jpg&name=large",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/cat-dance-gif-22123540.gif",
+      link: "https://www.youtube.com/shorts/CgbIEF1aF5A",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/cutie-pie-adorable-so-you-gif-14921579.gif",
+      link: "https://twitter.com/metmidnights/status/1736197823178190967",
+    },
+    { 
+      gif: "https://tenor.com/pt-BR/view/chuyfoxy-cat-kitten-angry-kitten-kicking-cat-gif-23206688.gif",
+      link: "https://x.com/PunchingCat/status/1753600271794712635?s=20",
+    },
+  ]
+}
